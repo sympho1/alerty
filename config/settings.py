faca40c5a5ec19f5e8cece272c1fe8a671f2c77c
sympho1 <symphognoboa@gmail.com>
2020-09-02 16:46:08 +0000

    changing password...

--- config/settings.py
+++ config/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'test',
-        'USER': 'yaday',
+        'USER': '',
         'PASSWORD': 'password',
         'HOST': 'localhost',
         'PORT': '',
